f14f1cb6bf237f5233c2bffdd18a7abc70cca1bc
srknssmn <srknssmn@gmail.com>
2023-10-19 23:22:19 +0300

    update

--- public/js/sauronHealth.js
+++ public/js/sauronHealth.js
@@ -15,6 +15,6 @@ export const sauronHealth = async () => {
     let totalLockedAmount2 = await totalLockedAmount1 * 1000;
     let totalLockedAmount3 = await Math.round(totalLockedAmount2)
     sauronHealthID.innerHTML = await totalLockedAmount3
-    let amount = await (1000000 - totalLockedAmount3) / 100000 * 100
+    let amount = await (1000000 - totalLockedAmount3) / 1000000 * 100
     sauronHealthP.style.width = `${amount}%`;
 };
